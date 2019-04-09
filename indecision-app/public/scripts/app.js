5774adb78cfa41c0d650e020858452c23e57f43d
Yuvaraj S <SYuvaraj@users.noreply.github.com>
2019-04-09 18:58:46 +0530

    const and let

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -8,7 +8,8 @@ console.log('App.js is running!');
 
 var app = {
     title: 'Indecision App',
-    subtitle: 'Put your life in the hands of a computer'
+    subtitle: 'Put your life in the hands of a computer',
+    options: ['option1', 'option2']
 };
 
 //JSX - JavaScript XM!
@@ -20,10 +21,15 @@ var template = React.createElement(
         null,
         app.title
     ),
-    React.createElement(
+    app.subtitle && React.createElement(
         'p',
         null,
         app.subtitle
+    ),
+    React.createElement(
+        'p',
+        null,
+        app.options.length > 0 ? 'Here are your options' : 'No options'
     ),
     React.createElement(
         'ol',
@@ -49,15 +55,23 @@ var user = {
 var userName = 'Mike';
 var userAge = 27;
 var userLocation = 'Bangalore';
+
+function getLocation(location) {
+    if (location) return React.createElement(
+        'p',
+        null,
+        location
+    );
+}
 var templateTwo = React.createElement(
     'div',
     null,
     React.createElement(
         'h1',
         null,
-        user.name
+        user.name ? user.name : 'Anonymous'
     ),
-    React.createElement(
+    user.age && user.age >= 18 && React.createElement(
         'p',
         null,
         'Age: ',
@@ -68,7 +82,8 @@ var templateTwo = React.createElement(
         null,
         'Location: ',
         user.location
-    )
+    ),
+    getLocation(user.location)
 );
 var appRoot = document.getElementById('app');
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -4,16 +4,18 @@ console.log('App.js is running!');
 //user title/subtitle in template
 //render template
 
-var app = {
+const app = {
     title: 'Indecision App',
-    subtitle: 'Put your life in the hands of a computer'
+    subtitle: 'Put your life in the hands of a computer',
+    options: ['option1','option2']
 };
 
 //JSX - JavaScript XM!
-var template = (
+const template = (
     <div>
         <h1>{app.title}</h1>
-        <p>{app.subtitle}</p>
+        {app.subtitle && <p>{app.subtitle}</p>}
+        <p>{app.options.length > 0 ? 'Here are your options' : 'No options'}</p>
         <ol>
             <li>Item onee</li>
             <li>Item two</li>
@@ -26,15 +28,21 @@ var user = {
     age: 26,
     location: 'Bangalore'
 }
-var userName = 'Mike';
-var userAge = 27;
-var userLocation = 'Bangalore';
-var templateTwo = (
+const userName = 'Mike';
+let userAge = 27;
+let userLocation = 'Bangalore';
+
+function getLocation(location){
+   if(location)
+    return <p>{location}</p>;
+}
+const templateTwo = (
     <div>
-        <h1>{user.name}</h1>
-        <p>Age: {user.age}</p>
+        <h1>{user.name ? user.name : 'Anonymous'}</h1>
+        { user.age && user.age >=18 && <p>Age: {user.age}</p> }
         <p>Location: {user.location}</p>
+        {getLocation(user.location)}
     </div>
 );
-var appRoot = document.getElementById('app');
-ReactDOM.render(template, appRoot);
+const appRoot = document.getElementById('app');
+ReactDOM.render(templateTwo, appRoot);
